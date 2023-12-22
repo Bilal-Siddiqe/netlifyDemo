f077b4b17fd2cb3e907f57b382287190a343eea9
Akhlaq <engr.akhlaq@hotmail.com>
2023-12-22 19:32:32 +0500

    Changes in App js

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Lorem, ipsum dolor.</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae at mollitia accusamus accusantium, tempore neque laborum quam incidunt eaque dicta delectus porro doloremque fuga odit cum voluptatibus id a beatae!</p>
+    </div>
+  );
+}
+
+export default App;
